e0f1daf03cae8dd3b2b71e4d382c463433cdd47a
Tim Ma <tim@wizmacau.com>
2017-08-03 15:51:06 +0800

    init a hello test file

---
+++ files/ips.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import time
+import signal
+import subprocess
+
+
+import scrollphathd
+from scrollphathd.fonts import font5x7
+
+print("""
+Scroll pHAT HD: Hello World
+
+Scrolls "Hello World" across the screen
+in a 5x7 pixel large font.
+
+Press Ctrl+C to exit!
+
+""")
+
+# Uncomment to rotate the text
+scrollphathd.rotate(180)
+
+# Set a more eye-friendly default brightness
+scrollphathd.set_brightness(0.1)
+
+msg = "Hello World! "
+scrollphathd.write_string(msg, x=0, y=0, font=font5x7)
+
+while True:
+    hostname = subprocess.check_output(['/bin/hostname', '-I'])
+    if msg != hostname:
+        msg = hostname
+        scrollphathd.clear()
+        scrollphathd.write_string(" IP: " + msg.strip(), x=0, y=0, font=font5x7)
+    scrollphathd.show()
+    scrollphathd.scroll()
+    time.sleep(0.02)
